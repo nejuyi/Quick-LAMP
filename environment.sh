2f9a488b8e48b98039ae9fdd9bb5f89e76d83271
Juan Jesús Bonilla <nejuyi@gmail.com>
2019-06-02 13:14:23 +0200

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -23,55 +23,56 @@ echo 'Keepassx'
 echo 'Xclip'
 echo 'GIMP'
 echo 'Composer'
+echo 'Terminator'
 echo '-----------------------------------------------------------------------------------'
 echo ''
 echo ''
 
 sudo apt update && sudo apt upgrade
 
-sudo apt-get install apache2 php7.0 mysql-server php7.0-mysql php7.0-gd php7.0-xml php7.0-json php7.0-mbstring mysql-workbench libapache2-mod-php7.0
+sudo apt-get install -y apache2 php7.2 php7.2-mysql php7.2-gd php7.2-xml php7.2-json php7.2-mbstring libapache2-mod-php7.2 mysql-server mysql-workbench 
 
 #VIM installer
- sudo apt-get instal vim
+ sudo apt-get install -y vim
 
 # #SUBLIME TEXT INSTALLER
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install -y sublime-text
+
+sudo apt-get install -y git git-flow
+
+sudo apt-get install -y terminator
+
+sudo apt-get install -y meld
+
+sudo apt-get install -y curl wget php-cli php-zip unzip
 
 #CHROMIUN BROWSER
-sudo apt-get install chromium-browser
+sudo apt-get install -y chromium-browser
 
 #SPOTIFY INSTALLER
 # 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 # 2. Add the Spotify repository
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 # 3. Update list of available packages
 sudo apt-get update
 # 4. Install Spotify
-sudo apt-get install spotify-client
-
+sudo apt-get install -y spotify-client
 
 # TELEGRAM INSTALLER
 sudo add-apt-repository ppa:atareao/telegram
 sudo apt-get update
-sudo apt install telegram
+sudo apt install -y telegram
 
+sudo apt-get install -y fonts-hack-ttf
 
-sudo apt-get install fonts-hack-ttf
+sudo apt-get install -y unity-tweak-tool
 
-sudo apt-get install unity-tweak-tool
-
-sudo apt-get install psensor
-
-sudo apt-get install git
-sudo apt-get install git-flow
-sudo apt-get install meld
-
-sudo apt-get install curl
+sudo apt-get install -y psensor
 
 #NODEJS INSTALLER
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
@@ -79,21 +80,22 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
 #KEEPASSX
-sudo apt-get install keepassx
+sudo apt-get install -y keepassx
+
 #Xclip
-sudo apt-get install xclip
+sudo apt-get install -y xclip
 
 #GIMP
-sudo apt-get install gimp
+sudo apt-get install -y gimp
 
 #composer
 cd ~
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/bin/composer 
-
 
 sudo apt-get autoremove
-sudo apt-get update
+
+
